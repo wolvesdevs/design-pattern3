24efc3c165217face754af712371a53cb106711b
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2023-11-20 22:14:50 +0900

    refs #B04_容器クラスにListを持たせてコンポジットパターンにする

---
+++ src/Rei01/Rei01/Buhin/Nakami.cs
@@ -0,0 +1,10 @@
+﻿namespace Rei01.Buhin
+{
+    internal sealed class Nakami : IBuhin
+    {
+        public void Execute()
+        {
+            MessageBox.Show("中身です。");
+        }
+    }
+}
---
+++ src/Rei01/Rei01/Buhin/Youki.cs
@@ -0,0 +1,21 @@
+﻿namespace Rei01.Buhin
+{
+    internal sealed class Youki : IBuhin
+    {
+        private List<IBuhin> _buhins = new List<IBuhin>(); 
+        public void Execute()
+        {
+            MessageBox.Show("容器です。");
+
+            foreach (var buhin in _buhins)
+            {
+                buhin.Execute();
+            }
+        }
+
+        public void Add(IBuhin buhin)
+        {
+            _buhins.Add(buhin);
+        }
+    }
+}
