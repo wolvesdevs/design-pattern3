fd2d3de57881702d23627976f9b6deef69460a9c
wolvesdevs_homePC <75551104+wolvesdevs@users.noreply.github.com>
2023-12-08 15:23:14 +0900

    refs #F03_AbstractFactoryとサブクラスの実装

---
+++ src/アブストラクトファクトリー_02/アブストラクトファクトリー/Factory/AbstractFactory.cs
@@ -0,0 +1,10 @@
+﻿using アブストラクトファクトリー.Data;
+
+namespace アブストラクトファクトリー.Factory
+{
+    public abstract class AbstractFactory
+    {
+        public abstract IProduct CreateProduct();
+        public abstract IStock CreateStock();
+    }
+}
---
+++ src/アブストラクトファクトリー_02/アブストラクトファクトリー/Factory/SqlServerFactory.cs
@@ -0,0 +1,17 @@
+﻿using アブストラクトファクトリー.Data;
+
+namespace アブストラクトファクトリー.Factory
+{
+    internal sealed class SqlServerFactory : AbstractFactory
+    {
+        public override IProduct CreateProduct()
+        {
+            return new ProductSqlServer();
+        }
+
+        public override IStock CreateStock()
+        {
+            return new StockSqlServer();
+        }
+    }
+}
