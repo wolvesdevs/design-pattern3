c2480611a75ee7e27326bb846f0158dd6436bfc7
wolvesdevs_homePC <75551104+wolvesdevs@users.noreply.github.com>
2023-12-09 12:31:46 +0900

    refs #F04_AbstractFactoryの効果を検証する

--- src/アブストラクトファクトリー_02/アブストラクトファクトリー/Factory/AbstractFactory.cs
+++ src/アブストラクトファクトリー_02/アブストラクトファクトリー/Factory/AbstractFactory.cs
@@ -6,5 +6,23 @@ namespace アブストラクトファクトリー.Factory
     {
         public abstract IProduct CreateProduct();
         public abstract IStock CreateStock();
+
+        public static AbstractFactory Create(int kind)
+        {
+            switch (kind)
+            {
+                case 0:
+#if DEBUG
+#else
+                    throw new ArgumentException("リリースモードでFakeは使えません");
+#endif
+                    return new FaceFactory();
+                case 1:
+                    return new SqlServerFactory();
+                default:
+                    throw new ArgumentException("kindがおかしい");
+            }
+        } 
+
     }
 }
--- src/アブストラクトファクトリー_02/アブストラクトファクトリー/Form1ViewModel.cs
+++ src/アブストラクトファクトリー_02/アブストラクトファクトリー/Form1ViewModel.cs
@@ -1,42 +1,41 @@
 ﻿using アブストラクトファクトリー.Data;
 using System.ComponentModel;
+using アブストラクトファクトリー.Factory;
 
 namespace アブストラクトファクトリー
 {
-  public class Form1ViewModel:INotifyPropertyChanged
-  {
-    private IProduct _product;
-    private IStock _stock;
-    public Form1ViewModel(
-      IProduct product,
-      IStock stock)
+    public class Form1ViewModel : INotifyPropertyChanged
     {
-      _product =  product;
-      _stock = stock;
+        private IProduct _product;
+        private IStock _stock;
+        public Form1ViewModel(AbstractFactory factory)
+        {
+            _product = factory.CreateProduct();
+            _stock = factory.CreateStock();
+        }
+
+        private string _label1Text = "--";
+        public string Label1Text
+        {
+            get { return _label1Text; }
+            set
+            {
+                _label1Text = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Label1Text"));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void GetButtonClick()
+        {
+            if (_stock.GetStock() < 1)
+            {
+                Label1Text = "在庫なし";
+                return;
+            }
+
+            Label1Text = _product.GetData() + "desu";
+        }
     }
-
-    private string _label1Text = "--";
-    public string Label1Text
-    {
-      get { return _label1Text; }
-      set
-      {
-        _label1Text = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Label1Text"));
-      }
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    public void GetButtonClick()
-    {
-      if (_stock.GetStock() < 1)
-      {
-        Label1Text = "在庫なし";
-        return;
-      }
-
-      Label1Text = _product.GetData() + "desu";
-    }
-  }
 }
