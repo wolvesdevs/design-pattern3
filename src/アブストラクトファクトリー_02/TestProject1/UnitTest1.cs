dcf4388045cba36d1ef403b5f380e793e8ec8009
wolvesdevs_homePC <75551104+wolvesdevs@users.noreply.github.com>
2023-12-10 14:14:14 +0900

    refs #F06_Moqを使ってAbstractFactoryのテストコードを書く方法

--- src/アブストラクトファクトリー_02/TestProject1/UnitTest1.cs
+++ src/アブストラクトファクトリー_02/TestProject1/UnitTest1.cs
@@ -1,37 +1,58 @@
+using Moq;
+using System.Diagnostics;
 using アブストラクトファクトリー;
 using アブストラクトファクトリー.Data;
+using アブストラクトファクトリー.Factory;
 
 namespace TestProject1
 {
-  [TestClass]
-  public class UnitTest1
-  {
-    [TestMethod]
-    public void TestMethod1()
+    [TestClass]
+    public class UnitTest1
     {
-      var mock = new ProductMock();
-      var stockMock = new StockMock();
-      var vm = new Form1ViewModel(mock, stockMock);
-      Assert.AreEqual("--", vm.Label1Text);
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var factoryMock = new FacoryMock();
+            var factoryMock = new Mock<AbstractFactory>();
+            var productMock = new Mock<IProduct>();
 
-      vm.GetButtonClick();
-      Assert.AreEqual("AAABBBdesu", vm.Label1Text);
-    }
-  }
+            factoryMock.Setup(x => x.CreateProduct()).Returns(productMock.Object);
+            productMock.Setup(x => x.GetData()).Returns("AAABBB");
 
-  internal sealed class ProductMock : IProduct
-  {
-    public string GetData()
-    {
-      return "AAABBB";
-    }
-  }
+            var vm = new Form1ViewModel(factoryMock.Object);
+            Assert.AreEqual("--", vm.Label1Text);
 
-  internal sealed class StockMock : IStock
-  {
-    public int GetStock()
-    {
-      return 1;
+            vm.GetButtonClick();
+            Assert.AreEqual("AAABBBdesu", vm.Label1Text);
+        }
     }
-  }
+
+    //internal sealed class FacoryMock : AbstractFactory
+    //{
+    //    public override IProduct CreateProduct()
+    //    {
+    //        return new ProductMock();
+    //    }
+
+    //    public override IStock CreateStock()
+    //    {
+    //        return new StockMock();
+    //    }
+    //}
+
+    //internal sealed class ProductMock : IProduct
+    //{
+    //    public string GetData()
+    //    {
+    //        return "AAABBB";
+    //    }
+    //}
+
+    //internal sealed class StockMock : IStock
+    //{
+    //    public int GetStock()
+    //    {
+    //        return 1;
+    //    }
+    //}
 }
